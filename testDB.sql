320af9b272d34c632fb23113ee0c58301725f591
Frances Lei Ramirez <86779163+leiramirez@users.noreply.github.com>
2022-03-03 13:30:24 +0800

    Set userID to auto increment

--- testDB.sql
+++ testDB.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `userinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `userinfo` (
-  `userID` int NOT NULL,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `firstName` varchar(45) NOT NULL,
   `lastName` varchar(45) NOT NULL,
   `emailAddress` varchar(45) NOT NULL,
