021fd6b1fd436ca5a534076410016c73bf07f790
j-askin <91054766+j-askin@users.noreply.github.com>
2022-03-04 12:13:42 +0800

    Add files via upload

--- js_files/AccountProfile.js
+++ js_files/AccountProfile.js
@@ -29,3 +29,41 @@ delete_overlay.addEventListener("click", () => {
     delete_overlay.classList.remove("active")
     msg.classList.remove("active")
 })
+
+// for changing acc details
+function validate() {
+    let fname = document.getElementById("fname");
+    let lname = document.getElementById("lname");
+    let birthday = document.getElementById("birthday");
+    let password = document.getElementById("password");
+    let confPassword = document.getElementById("confPassword");
+  
+    var isAlpha = /^[A-Za-z\s]*$/;
+  
+    var dob = new Date(birthday.value);
+    var diff = Date.now() - dob.getTime();
+    var date_diff = new Date(diff);
+    var age = date_diff.getUTCFullYear() - 1970;
+  
+    if (!(fname.value.match(isAlpha))) {
+      alert("Error! Your first name is not valid.");
+      return false;
+    }
+  
+    if (!(lname.value.match(isAlpha))) {
+      alert("Error! Your last name is not valid.");
+      return false;
+    }
+  
+    if(age < 16 || age > 26) {
+      alert("Error! Please make sure you are a college student before creating an account.");
+      return false;
+    }
+  
+    if (password.value != confPassword.value) {
+      alert("Error! Passwords do not match.");
+      return false;
+    }
+  
+    return true;
+  }
