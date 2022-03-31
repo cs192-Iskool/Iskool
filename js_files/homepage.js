e1bc6d183690b8bc693931aac5701ad1af1a0ab2
j-askin <91054766+j-askin@users.noreply.github.com>
2022-03-31 23:17:54 +0800

    Added script for clickable thumbnail

--- js_files/homepage.js
+++ js_files/homepage.js
@@ -10,3 +10,18 @@ input.addEventListener("keyup", function(event) {
     document.getElementById("search_ad").click();
   }
 });
+
+function show_thumbnail(id) {
+  const pic_overlay = document.getElementById("pic_overlay");
+  const pic = document.getElementById("pic");
+  const view_pic = document.getElementById(id);
+
+  pic_overlay.classList.add("active");
+  pic.style.display = "block";
+  pic.src = view_pic.src;
+
+  pic_overlay.addEventListener("click", () => {
+      pic_overlay.classList.remove("active");
+      pic.style.display = "none";
+})
+}
