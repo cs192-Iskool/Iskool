9a5ca07c5c9dea519a78dff8dd6843577087baab
Frances Lei Ramirez <86779163+leiramirez@users.noreply.github.com>
2022-05-11 23:39:25 +0800

    Bug fix for empty string messages

--- js_files/sendMessage.js
+++ js_files/sendMessage.js
@@ -1,6 +1,10 @@
 var input = document.getElementById("message");
 input.addEventListener("keyup", function(event) {
   if (event.key === "Enter" && !event.shiftKey) {
+    if (input.value.length <= 1) {
+      input.value = "";
+      return;
+    }
     event.preventDefault();
     document.getElementById("send_message").click();
   }
