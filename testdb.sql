fc4bfdc5037a08e7b1b65ef87f355175ef82b4b7
Frances Lei Ramirez <86779163+leiramirez@users.noreply.github.com>
2022-05-11 15:52:00 +0800

    Corrected database

--- testdb.sql
+++ testdb.sql
@@ -1,184 +1,193 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `testdb`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: May 11, 2022 at 09:14 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `testdb2`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `activechats`
 --
 
+DROP TABLE IF EXISTS `activechats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `activechats` (
-  `chatID` int(11) NOT NULL,
+  `chatID` int(11) NOT NULL AUTO_INCREMENT,
   `tutorID` int(11) NOT NULL,
   `tuteeID` int(11) NOT NULL,
-  `subject` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `subject` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`chatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `activechats`
+--
+
+LOCK TABLES `activechats` WRITE;
+/*!40000 ALTER TABLE `activechats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activechats` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `adinfo`
 --
 
+DROP TABLE IF EXISTS `adinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `adinfo` (
-  `userID` int(11) NOT NULL,
-  `adID` int(11) NOT NULL,
-  `subject` varchar(45) NOT NULL,
-  `price` int(11) NOT NULL,
-  `image` blob DEFAULT NULL,
-  `timeCreated` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `userID` int NOT NULL,
+  `adID` int NOT NULL AUTO_INCREMENT,
+  `subject` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `price` int NOT NULL,
+  `image` blob,
+  `timeCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`adID`),
+  KEY `userID` (`userID`),
+  CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `userinfo` (`userID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `adinfo`
+--
+
+LOCK TABLES `adinfo` WRITE;
+/*!40000 ALTER TABLE `adinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adinfo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `bookings`
 --
 
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bookings` (
-  `bookingID` int(11) NOT NULL,
-  `tuteeID` int(11) NOT NULL,
-  `tutorID` int(11) NOT NULL,
-  `subject` varchar(45) NOT NULL,
-  `unread` int(11) NOT NULL DEFAULT 1,
-  `timeCreated` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `bookingID` int NOT NULL AUTO_INCREMENT,
+  `tuteeID` int NOT NULL,
+  `tutorID` int NOT NULL,
+  `subject` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `unread` int NOT NULL DEFAULT '1',
+  `timeCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bookingID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `messages`
+-- Dumping data for table `bookings`
 --
 
-CREATE TABLE `messages` (
-  `chatID` int(11) NOT NULL,
-  `senderID` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `timeCreated` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `notifs`
 --
 
+DROP TABLE IF EXISTS `notifs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `notifs` (
-  `targetUserID` int(11) NOT NULL,
-  `bookingID` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `subject` varchar(45) NOT NULL,
-  `sourceUserID` int(11) NOT NULL,
-  `timeCreated` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `targetUserID` int NOT NULL,
+  `bookingID` int NOT NULL,
+  `status` int NOT NULL,
+  `subject` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `sourceUserID` int NOT NULL,
+  `timeCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `messages` (
+  `chatID` int(11) NOT NULL,
+  `senderID` int(11) NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  `timeCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `chatID` (`chatID`),
+  CONSTRAINT `chatID` FOREIGN KEY (`chatID`) REFERENCES `activechats` (`chatid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `notifs`
+--
+
+LOCK TABLES `notifs` WRITE;
+/*!40000 ALTER TABLE `notifs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notifs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `userinfo`
 --
 
+DROP TABLE IF EXISTS `userinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `userinfo` (
-  `userID` int(11) NOT NULL,
-  `firstName` varchar(45) NOT NULL,
-  `lastName` varchar(45) NOT NULL,
-  `emailAddress` varchar(45) NOT NULL,
-  `campus` varchar(20) NOT NULL,
-  `course` varchar(45) NOT NULL,
-  `yearStanding` varchar(20) NOT NULL,
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `emailAddress` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `campus` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `course` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `yearStanding` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   `birthday` date NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `profPic` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `profPic` int DEFAULT '0',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `userinfo`
 --
 
---
--- Indexes for table `activechats`
---
-ALTER TABLE `activechats`
-  ADD PRIMARY KEY (`chatID`);
+LOCK TABLES `userinfo` WRITE;
+/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `adinfo`
---
-ALTER TABLE `adinfo`
-  ADD PRIMARY KEY (`adID`),
-  ADD KEY `userID` (`userID`);
-
---
--- Indexes for table `bookings`
---
-ALTER TABLE `bookings`
-  ADD PRIMARY KEY (`bookingID`);
-
---
--- Indexes for table `userinfo`
---
-ALTER TABLE `userinfo`
-  ADD PRIMARY KEY (`userID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `activechats`
---
-ALTER TABLE `activechats`
-  MODIFY `chatID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `adinfo`
---
-ALTER TABLE `adinfo`
-  MODIFY `adID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `bookings`
---
-ALTER TABLE `bookings`
-  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `userinfo`
---
-ALTER TABLE `userinfo`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `adinfo`
---
-ALTER TABLE `adinfo`
-  ADD CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `userinfo` (`userID`) ON DELETE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-26  0:08:00
